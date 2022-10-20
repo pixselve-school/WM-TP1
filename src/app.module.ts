6af318d2b93b229ebba7b6dbafc5adc314756542
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-10-20 15:02:17 +0200

    chore: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AssociationsController } from './associations/associations.controller';
 import { AssociationsModule } from './associations/associations.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AssociationsModule],
+  imports: [
+    UsersModule,
+    AssociationsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'mydatabase.db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
