9a30ca42f28965faafc1c0c3e16f739235e5589c
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-10-22 20:06:50 +0200

    feat(minutes): create

--- src/minutes/minutes.service.ts
+++ src/minutes/minutes.service.ts
@@ -1,11 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMinuteDto } from './dto/create-minute.dto';
 import { UpdateMinuteDto } from './dto/update-minute.dto';
+import { Repository } from 'typeorm';
+import { Minute } from './entities/minute.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AssociationsService } from '../associations/associations.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class MinutesService {
-  create(createMinuteDto: CreateMinuteDto) {
-    return 'This action adds a new minute';
+  constructor(
+    @InjectRepository(Minute) private readonly repository: Repository<Minute>,
+    private readonly associationsService: AssociationsService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  /**
+   * Create a new minute.
+   * @param createMinuteDto the minute to create
+   * @returns the created minute
+   * @throws {NotFoundException} if the association or the users does not exist
+   */
+  async create(createMinuteDto: CreateMinuteDto): Promise<Minute> {
+    const [association, users] = await Promise.all([
+      this.associationsService.findOne(createMinuteDto.idAssociation),
+      this.usersService.findManyById(createMinuteDto.idVoters),
+    ]);
+    if (association === null) {
+      throw new NotFoundException('Association not found');
+    }
+    if (users.length !== createMinuteDto.idVoters.length) {
+      throw new NotFoundException('Some users not found');
+    }
+
+    const minute = new Minute();
+    minute.content = createMinuteDto.content;
+    minute.date = new Date(createMinuteDto.date);
+    minute.association = association;
+    minute.voters = users;
+    return this.repository.save(minute);
   }
 
   findAll() {
