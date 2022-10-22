f4e4a16aea8419a5efc496532927253948e125df
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-10-22 20:06:34 +0200

    chore(minutes): imports

--- src/minutes/minutes.module.ts
+++ src/minutes/minutes.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MinutesService } from './minutes.service';
 import { MinutesController } from './minutes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Minute } from './entities/minute.entity';
+import { AssociationsModule } from '../associations/associations.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   controllers: [MinutesController],
-  providers: [MinutesService]
+  providers: [MinutesService],
+  imports: [
+    TypeOrmModule.forFeature([Minute]),
+    AssociationsModule,
+    UsersModule,
+  ],
 })
 export class MinutesModule {}
