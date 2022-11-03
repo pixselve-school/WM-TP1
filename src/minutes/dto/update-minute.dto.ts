1c0e5db8945401694ee9b2bfd6a7e839725a1df6
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-03 16:29:33 +0100

    chore(minutes): required false

--- src/minutes/dto/update-minute.dto.ts
+++ src/minutes/dto/update-minute.dto.ts
@@ -7,28 +7,32 @@ export class UpdateMinuteDto {
     example:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et sagittis sem. Praesent sollicitudin lacus.',
     type: String,
+    required: false,
   })
-  public content: string;
+  public content?: string;
 
   @ApiProperty({
     description:
       'The ids of the voters. These should be the same than the users that are members of the association',
     example: [1, 2, 3],
     type: [Number],
+    required: false,
   })
-  public idVoters: number[];
+  public idVoters?: number[];
 
   @ApiProperty({
     description: 'The date when the general assembly occured',
     example: '12/12/2021',
     type: String,
+    required: false,
   })
-  public date: string;
+  public date?: string;
 
   @ApiProperty({
     description: 'The id of the association',
     example: 1,
     type: Number,
+    required: false,
   })
-  public idAssociation: number;
+  public idAssociation?: number;
 }
