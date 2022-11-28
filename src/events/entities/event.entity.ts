1139f925b1e7f611419ba19ab3f62d227144222c
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-11-28 15:37:30 +0100

    feat(events): create entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,21 @@
-export class Event {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import Association from '../../associations/entities/association.entity';
+
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  start: Date;
+
+  @Column()
+  end: Date;
+
+  @Column()
+  @ManyToOne(() => Association, (object) => object.id)
+  association: Association;
+}
