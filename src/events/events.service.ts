a7dbaeb6d62cf5774932e160026b6fa1a6bf75dd
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-11-28 15:44:27 +0100

    feat(events): import repository

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from './entities/event.entity';
 
 @Injectable()
 export class EventsService {
+  constructor(
+    @InjectRepository(Event) private readonly repository: Repository<Event>,
+  ) {}
+
   create(createEventDto: CreateEventDto) {
     return 'This action adds a new event';
   }
