eb00306b3f92bb36984048ac3a6b5ca93a2943dd
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-11-28 19:06:44 +0100

    feat(events): added contraints

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateEventDto {
   @ApiProperty({
@@ -6,20 +7,25 @@ export class CreateEventDto {
     example: "Romain's birthday",
     type: String,
   })
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({
     description: 'The start date of the event',
     example: '2021-01-01T00:00:00.000Z',
-    type: Date,
+    type: String,
   })
+  @IsNotEmpty()
+  @IsDateString()
   start: Date;
 
   @ApiProperty({
     description: 'The end date of the event',
     example: '2021-01-01T00:00:00.000Z',
-    type: Date,
+    type: String,
   })
+  @IsNotEmpty()
+  @IsDateString()
   end: Date;
 
   @ApiProperty({
@@ -27,5 +33,6 @@ export class CreateEventDto {
     example: 1,
     type: Number,
   })
+  @IsNumber()
   association: number;
 }
