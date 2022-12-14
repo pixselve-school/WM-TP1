326b1087d737da9f536c5718486969e7b5cb5450
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2022-12-14 18:37:34 +0100

    feat: include ID in AssociationMember

--- src/associations/dto/association.member.ts
+++ src/associations/dto/association.member.ts
@@ -6,12 +6,14 @@ export class AssociationMember {
   public firstname: string;
   public age: number;
   public role: string;
+  public id: number;
 
   from(user: User, role: Role): AssociationMember {
     this.lastname = user.lastname;
     this.firstname = user.firstname;
     this.age = user.age;
     this.role = role ? role.name : 'member';
+    this.id = user.id;
     return this;
   }
 }
